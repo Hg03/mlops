1ff5a62ad82a8ee6464e10a0f764cab99614f145
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2024-08-03 13:59:36 +0530

    Update training.py

--- mlops/pipelines/training.py
+++ mlops/pipelines/training.py
@@ -1,7 +1,9 @@
 from mlops.my_hopworks.connector import FeatureStoreManager
 from config import load_config
 from dotenv import load_dotenv
+from comet_ml import Experiment
 import polars as pl
+from xgboost import XGBClassifier
 import os
 
 class training_pipeline:
@@ -22,7 +24,25 @@ class training_pipeline:
         return self.train, self.test
     
     def tune_and_train(self):
-        ...
+        comet_ml_api_key = os.getenv("comet_ml_api_key")
+        experiment = Experiment(api_key=comet_ml_api_key, project_name="purchase-prediction")
+        X_train, y_train = self.train.drop("purchasestatus"), self.train.select("purchasestatus")
+        X_test, y_test = self.test.drop("purchasestatus"), self.test.select("purchasestatus")
+        model = XGBClassifier(n_estimators=10, max_depth=5, learning_rate=0.1, objective="binary:logistic")
+        # fit model
+        model.fit(X_train,y_train,eval_set=[(X_test, y_test)])
+        # make predictions
+        y_pred = model.predict(X_test)
+        debug_df = pl.DataFrame(X_test)
+
+        debug_df["pred"] = y_pred
+        debug_df["ground_truth"] = y_test
+
+        experiment.log_table("prediction_debug_table.csv", debug_df)
+        print('logged !!')
+
+        
+        
         
     def run(self):
         train, test = self.load_from_hopsworks()
