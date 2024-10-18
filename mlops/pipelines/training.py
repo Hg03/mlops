df09f2b1ee00eead4081fff1d7fdc8e682df414e
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2024-10-18 21:28:37 +0530

    Update training.py

--- mlops/pipelines/training.py
+++ mlops/pipelines/training.py
@@ -1,11 +1,13 @@
+from comet_ml import Experiment, API
 from mlops.my_hopworks.connector import FeatureStoreManager
 from config import load_config
 from dotenv import load_dotenv
-from comet_ml import Experiment
 import polars as pl
 from xgboost import XGBClassifier
 import joblib
-from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+import secrets
+import string
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score, precision_recall_curve
 import os
 
 class training_pipeline:
@@ -13,6 +15,10 @@ class training_pipeline:
         self.configs = load_config()
         self.current_time = current_time
         self.path = path
+    
+    def generate_api_key(self, length: int = 32) -> str:
+        alphabet = string.ascii_letters + string.digits
+        return "".join(secrets.choice(alphabet) for _ in range(length))
     
     def load_from_hopsworks(self):
         load_dotenv()
@@ -28,7 +34,8 @@ class training_pipeline:
     
     def tune_and_train(self):
         comet_ml_api_key = os.getenv("comet_ml_api_key")
-        experiment = Experiment(api_key=comet_ml_api_key, project_name="purchase-prediction")
+        exp_key = self.generate_api_key()
+        experiment = Experiment(api_key=comet_ml_api_key, project_name="purchase-prediction", workspace='harish-workspace', experiment_key=exp_key)
         X_train = self.train.drop("purchasestatus")
         y_train = self.train["purchasestatus"]
         X_test = self.test.drop("purchasestatus")
@@ -59,13 +66,34 @@ class training_pipeline:
             "recall": recall_score(y_test_series, y_pred_series),
             "roc_auc": roc_auc_score(y_test_series, y_pred_series)
             }
+                
         experiment.log_metrics(scores)
+        experiment.log_confusion_matrix(y_test_series, y_pred_series)
+        precision, recall, _ = precision_recall_curve(y_test_series, y_pred_series)
+
+        # Log the curve to Comet
+        experiment.log_curve(
+            name="precision-recall",
+            x=precision,
+            y=recall
+        )
         # Add prediction and ground truth columns to the test DataFrame
         debug_df = X_test.with_columns([y_pred_series, y_test_series])
         # Log the DataFrame as a CSV to Comet.ml
         experiment.log_table("prediction_debug_table.csv", debug_df.to_pandas())
-        joblib.dump(model, os.path.join(self.path["output"], self.configs.path.model_file))
+        joblib.dump(model, os.path.join(self.path["model"], self.configs.path.model_file))
         experiment.log_model("model", os.path.join(self.path["model"], self.configs.path.model_file))
+        # experiment.register_model("model", version='1')
+        # Register the model in the model registry
+        api = API(api_key=comet_ml_api_key)
+        
+        api_experiment = api.get(f'harish-workspace/purchase-prediction/{exp_key}')
+        api_experiment.register_model("model")
+        #     model_name='model',
+        #     version='1.0.0',
+        #     comment='Initial model registration',
+        #     file_name=os.path.join(self.path["model"], self.configs.path.model_file)
+        #     )
         print('logged !!')
 
 
@@ -77,3 +105,5 @@ class training_pipeline:
 
 if __name__ == "__main__":
     pass
+
+        
